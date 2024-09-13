b0f9fa3736a05b81df53b10ddd54cd30f6c50ee7
VoVcHiC <tsvetkovvova17@gmail.com>
2024-09-14 02:50:38 +0300

    add telethon example

---
+++ docs/examples/telethon.rst
@@ -0,0 +1,13 @@
+========
+Telethon
+========
+
+Usage example with `Telethon <https://docs.telethon.dev/en/stable/>`_
+--------------------------------------------------------
+
+.. literalinclude:: ../../examples/telethon_example.py
+
+Preview
+-------
+
+.. image:: ../_static/payment_handle_example.png
---
+++ examples/telethon_example.py
@@ -0,0 +1,36 @@
+from telethon import TelegramClient, events
+from telethon.events.newmessage import NewMessage
+
+from cryptopay import CryptoPay
+from cryptopay.types import Invoice
+
+api_id = 12345678
+api_hash = "API HASH"
+
+client = TelegramClient("anon", api_id, api_hash)
+cp = CryptoPay("TOKEN")
+
+
+@client.on(events.NewMessage(func=lambda e: e.is_private))
+async def get_invoice(message: NewMessage.Event) -> None:
+    invoice = await cp.create_invoice(1, "USDT")
+    await client.send_message(
+        message.chat_id,
+        f"pay: {invoice.mini_app_invoice_url}",
+    )
+    invoice.await_payment(chat_id=message.chat_id)
+
+
+@cp.polling_handler()
+async def handle_payment(
+    invoice: Invoice,
+    chat_id: int,
+) -> None:
+    await client.send_message(
+        chat_id,
+        f"payment received: {invoice.amount} {invoice.asset}",
+    )
+
+
+client.start()
+cp.run_polling()
