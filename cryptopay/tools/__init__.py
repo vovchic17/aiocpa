406dcd1090e9e5f74fbf210708fd1d4dd3c18932
VoVcHiC <tsvetkovvova17@gmail.com>
2024-11-13 08:21:34 +0300

    remove _run_sync_async func

--- cryptopay/tools/__init__.py
+++ cryptopay/tools/__init__.py
@@ -1,17 +1,7 @@
-import asyncio
-import inspect
-from typing import TYPE_CHECKING
-
 from .delete_all_checks import DeleteAllChecks
 from .delete_all_invoices import DeleteAllInvoices
 from .exchange import Exchange
 from .get_balance_by_asset import GetBalanceByAsset
-
-if TYPE_CHECKING:
-    from collections.abc import Callable
-    from typing import Any, ParamSpecArgs, ParamSpecKwargs
-
-    import cryptopay
 
 
 class Tools(
@@ -19,18 +9,7 @@ class Tools(
     DeleteAllInvoices,
     Exchange,
     GetBalanceByAsset,
-):
-    def _run_sync_async(
-        self: "cryptopay.CryptoPay",
-        method: "Callable[..., Any]",
-        *args: "ParamSpecArgs",
-        **kwargs: "ParamSpecKwargs",
-    ) -> object:
-        coro = method(*args, **kwargs)
-        if inspect.iscoroutinefunction(method):
-            loop = asyncio.get_event_loop()
-            return loop.run_until_complete(coro)
-        return coro
+): ...
 
 
 __all__ = (
