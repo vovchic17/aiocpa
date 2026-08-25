d17bab88c371f1165879c533347cc0fa873f297d
VoVcHiC <tsvetkovvova17@gmail.com>
2026-08-25 03:32:08 +0300

    add docstring

--- aiosend/__init__.py
+++ aiosend/__init__.py
@@ -1,3 +1,21 @@
+"""
+       .@@. .*.
+     .@@@++@@@@@.                 .                                       ..
+   .#@@++@@@@@@@@@.              #@                                       @@
+ .#@@++@@@@@@@@@@@@      .@@@+.  **   *@@@.  .@@@@   *@@@.   .*@+.   .*@@*@@
+*@@++++#@@@@@@@@@@%     @@   @@. #@ .@@  #@% @@    .@%  #@% @@@ #@+ @@@  #@@
+ -+++++++@@@@@@@@       @@    @@ #@ #@    @@  =@@@ #@@@@@@@ @@   @@ @@    @@
+   ++++++++@@@@         @@@.-@@@ #@  @@..@@% .+  @% @@   _  @@   @@ #@@..@@%
+     +++++++@             #== == ==    ==%    #==    %==%   =%   ==   #==%
+       ++++
+
+aiosend is a synchronous & asynchronous Crypto Pay API client.
+
+It provides a fully typed interface for working with the Crypto Pay API,
+along with event routing, polling, webhook handling, filters, dependency
+injection, and convenient utilities for building Crypto Pay integrations.
+"""
+
 from aiosend._events import PayloadData
 from aiosend._utils.sync import CryptoPay
 from aiosend.client import MAINNET, TESTNET
